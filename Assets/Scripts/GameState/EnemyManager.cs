752f486df7f741e026654078411250eb7984ed80
Erto Pena <ertopena@gmail.com>
2016-01-10 00:33:15 -0600

    Add EnemyManager file (still needs to be filled in).

--- Assets/Scripts/GameState/EnemyManager.cs
+++ Assets/Scripts/GameState/EnemyManager.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EnemyManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	public GameObject marcherPrefab;
+	public GameObject spinnerPrefab;
+	public GameObject bomberPrefab;
+
+
+	private GameObject[] marchers;
+	private GameObject[] spinners;
+	private GameObject[] bombers;
+
+
+	// TODO: Seed enemies. Write method to set to active one by one, for the gameController's use.
 }
