f3dd04a78d49c6b426c16099a1900d6363160f66
Erto Pena <ertopena@gmail.com>
2016-01-10 16:01:23 -0600

    Add some game state handling to the GameController.

--- Assets/Scripts/GameState/GameController.cs
+++ Assets/Scripts/GameState/GameController.cs
@@ -4,33 +4,61 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 
 	public delegate void GameStateEvent();
-	public event GameStateEvent OnLevelStart;
-	public event GameStateEvent OnLevelComplete;
+	public event GameStateEvent OnWaveStart;
+	public event GameStateEvent OnSpawningCompleted;
+	public event GameStateEvent OnWaveCleared;
 	public event GameStateEvent OnPlayerDeath;
 
 
+	public GameState State { get; private set; }
+
 	public HeartRateTracker heartRateTracker;
-
-
 	public float childRadius = 0.5f;
+
+
+	void Awake()
+	{
+		State = GameState.PrePlay;
+	}
 
 
 	void OnEnable()
 	{
 		heartRateTracker.OnLethalHeartRate += HandlePlayerDeath;
+		EnemyCounter.OnEnemyCountUpdate += HandleEnemyCountChange;
 	}
 
 
 	void OnDisable()
 	{
 		heartRateTracker.OnLethalHeartRate -= HandlePlayerDeath;
+		EnemyCounter.OnEnemyCountUpdate -= HandleEnemyCountChange;
 	}
 
 
-	void StartLevel()
+	void StartWave()
 	{
-		if (OnLevelStart != null)
-			OnLevelStart();
+		if (OnWaveStart != null)
+			OnWaveStart();
+	}
+
+
+	void HandleSpawningCompleted()
+	{
+		// TODO
+	}
+
+
+	void HandleEnemyCountChange(int newCount)
+	{
+		if (newCount == 0)
+			HandleWaveCleared();
+	}
+
+
+	void HandleWaveCleared()
+	{
+
 	}
 
 
