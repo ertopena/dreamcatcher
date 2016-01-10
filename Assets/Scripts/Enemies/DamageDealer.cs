dc67f05bfdd5c5fde8a3c01cb2d187a7f9090c3a
Erto Pena <ertopena@gmail.com>
2016-01-09 21:34:30 -0600

    Flesh out DamageDealer to work with EnemyMotion.

--- Assets/Scripts/Enemies/DamageDealer.cs
+++ Assets/Scripts/Enemies/DamageDealer.cs
@@ -4,8 +4,21 @@ using System.Collections;
 public class DamageDealer : MonoBehaviour {
 
 	public delegate void DamageEvent(DamageDealer dealer, int damageDealt);
-	public static event DamageEvent OnDamageDealt;
+	public static event DamageEvent OnDamageToChildAttempted;
 
 
 	public int standardDamage = 40;
+
+
+	public void DamageChild()
+	{
+		OnDamageToChildAttempted(this, EffectiveDamage());
+	}
+
+
+	int EffectiveDamage()
+	{
+		// TODO: Expand to add periods of reduced damage or invincibility.
+		return standardDamage;
+	}
 }
